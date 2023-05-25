362dc26bff8a3c06011e405d5adcb1dbcdda0f18
Ryan Peters <ryanpeters@microsoft.com>
2023-05-24 20:06:47 -0400

    cleaned home controller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,18 +1,9 @@
-﻿using System.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
-using LessCompile.Models;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace LessCompile.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
-
     public IActionResult Index()
     {
         return View();
